3f7ffcaf7c30ea88152f99a4d9e5d5d9af30d5f3
Hiba <mahamidheba@gmail.com>
2026-07-15 22:41:43 +0300

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import Header from "./components/layout/Header";
 function App() {
-  return <>App</>;
+  return (
+    <div className="min-h-svh max-w-4xl mx-auto flex flex-col p-4 bg-[#f1f1f1]">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,42 @@
+import { Cloud, Sun } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between">
+      {/* logo */}
+      <div
+        className="flex items-center gap-1 text-blue-400 text-lg"
+        aria-label="Nasma"
+      >
+        <span>
+          <Cloud className="size-6" strokeWidth={3} />
+        </span>
+        <h3 className="font-semibold md:text-xl">Nasma</h3>
+      </div>
+      {/* === logo === */}
+
+      {/* controls */}
+      <div className="flex gap-3">
+        {/* temp unit */}
+        <div className="flex items-center gap-1 rounded-4xl py-0.5 px-4 text-gray-600 bg-blue-300 border border-gray-200">
+          <button type="button">°C</button>
+          <span className="mx-1">/</span>
+          <button type="button">°F</button>
+        </div>
+        {/* === temp unit === */}
+
+        {/* theme toggle */}
+        <button
+          className="bg-blue-900 text-yellow-400 border border-gray-200 p-2.5 rounded-full"
+          type="button"
+        >
+          <Sun className="size-4" />
+        </button>
+        {/* === theme toggle === */}
+      </div>
+      {/* === controls === */}
+    </header>
+  );
+};
+
+export default Header;
